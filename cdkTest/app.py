532dc3d680402ba069a4767107059abc1ce0c240
tirth pipalia <56732761+tirth-pipalia@users.noreply.github.com>
2022-03-10 23:23:24 +0000

    Update app.py

--- cdkTest/app.py
+++ cdkTest/app.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 
-import aws_cdk as cdk
-
+from aws_cdk import (
+    App
+)
+import os
 from cdk_test.cdk_test_stack import CdkTestStack
 
-
-app = cdk.App()
-CdkTestStack(app, "cdk-test")
+app = App()
+CdkTestStack(app, "cdk-test", env={
+    'account': os.environ['CDK_DEFAULT_ACCOUNT'],
+    'region': os.environ['CDK_DEFAULT_REGION']
+  })
 
 app.synth()
